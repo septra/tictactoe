b53fe0016e31994925b3d44874aaf01ef1dad067
Sahil Singh <sahilsingh90@gmail.com>
2016-09-03 18:19:55 +0530

    Added winning check and game over flag. V1 done.

--- game.py
+++ game.py
@@ -9,12 +9,43 @@ class Game(object):
                      [None, None, None]]
         self.turn = random.choice([self.xs, self.os])
 
+        self.over = False
+
         print("It's %s's turn to begin" % self.turn)
 
     def play(self, row, col):
-        self.grid[row][col] = self.turn
-        self.turn = self.xs if self.turn == self.os else self.os
+
+        if self.over is True:
+           print("Game is Over! Start a new one.")
+           return None
+
+        # To convert into 0 based index
+        row -= 1
+        col -= 1
+
+        if self.grid[row][col]:
+            raise AssignmentError("Position already taken!")
+        else:
+            self.grid[row][col] = self.turn
+
+            self.check()
+
+            self.turn = self.xs if self.turn == self.os else self.os
+
         print(self)
+
+
+    def check(self):
+        if None != self.grid[0][0] == self.grid[0][1] == self.grid[0][2] or \
+           None != self.grid[1][0] == self.grid[1][1] == self.grid[1][2] or \
+           None != self.grid[2][0] == self.grid[2][1] == self.grid[2][2] or \
+           None != self.grid[0][0] == self.grid[1][0] == self.grid[2][0] or \
+           None != self.grid[0][1] == self.grid[1][1] == self.grid[2][1] or \
+           None != self.grid[0][2] == self.grid[1][2] == self.grid[2][2] or \
+           None != self.grid[0][0] == self.grid[1][1] == self.grid[2][2] or \
+           None != self.grid[0][2] == self.grid[1][1] == self.grid[2][0]:
+               self.over = True
+               print("Game Over! %s Wins." % self.turn)
 
 
     def __str__(self):
@@ -39,6 +70,11 @@ class Game(object):
 
         return "\n".join(output)
 
+
+class AssignmentError(Exception):
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message
 
 
 
