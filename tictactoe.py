66168ebad1c97782032338fbf22883f79e578140
Sahil Singh <sahilsingh90@gmail.com>
2017-05-20 04:38:19 +0530

    Made game module executable

---
+++ tictactoe.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+import random
+
+
+class Game(object):
+    def __init__(self):
+        self.xs = 'X'
+        self.os = 'O'
+        self.grid = [[None, None, None],
+                     [None, None, None],
+                     [None, None, None]]
+        self.turn = random.choice([self.xs, self.os])
+
+        self.over = False
+
+        # print("It's %s's turn to begin" % self.turn)
+
+    def play(self, row, col):
+
+        if self.over is True:
+            print("Game is Over! Start a new one.")
+            return None
+
+        # To convert into 0 based index
+        row -= 1
+        col -= 1
+
+        if self.grid[row][col]:
+            raise AssignmentError("Position already taken!")
+        else:
+            self.grid[row][col] = self.turn
+
+            self.check()
+
+            self.turn = self.xs if self.turn == self.os else self.os
+
+        return True
+
+    def check(self):
+        if (None != self.grid[0][0] == self.grid[0][1] == self.grid[0][2] or
+            None != self.grid[1][0] == self.grid[1][1] == self.grid[1][2] or
+            None != self.grid[2][0] == self.grid[2][1] == self.grid[2][2] or
+            None != self.grid[0][0] == self.grid[1][0] == self.grid[2][0] or
+            None != self.grid[0][1] == self.grid[1][1] == self.grid[2][1] or
+            None != self.grid[0][2] == self.grid[1][2] == self.grid[2][2] or
+            None != self.grid[0][0] == self.grid[1][1] == self.grid[2][2] or
+            None != self.grid[0][2] == self.grid[1][1] == self.grid[2][0]):
+                self.over = True
+                print("Game Over! %s Wins." % self.turn)
+
+    def __str__(self):
+        output = []
+        for row in self.grid:
+            output.append("{:^3}|{:^3}|{:^3}".format(
+                    *[piece if piece is not None else ' ' for piece in row]))
+
+        return "\n".join(output)
+
+    def __repr__(self):
+        output = []
+        for row in self.grid:
+            output.append("{:^3}|{:^3}|{:^3}".format(
+                    *[piece if piece is not None else ' ' for piece in row]))
+
+        return "\n".join(output)
+
+
+class AssignmentError(Exception):
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message
+
+
+if __name__ == '__main__':
+
+    print("Enter position to play in [r,c] format\n" +
+          "Ex:\t>> 1,2\n")
+
+    game = Game()
+
+    print(game)
+    print("It's %s's turn to begin" % game.turn)
+
+    ongoingGame = True
+
+    while ongoingGame:
+        if game.over: break
+        playerInput = raw_input("%s> " % game.turn)
+        print("")
+        playerInput = playerInput.split(",")
+        row, col = int(playerInput[0]), int(playerInput[1])
+        ongoingGame = game.play(row, col)
+        print(game)
+        print("")
